453756ac67a791c70e504e1cc381af2b10e83a4c
Harshs1ngh <singharsh.cse@gmail.com>
2025-12-25 11:40:48 +0530

    fixed paths and moved project to src

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const Review = require("../models/review");
+const Review = require("../models/review.js");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const Listing = require("../models/Listing");
+const Listing = require("../models/Listing.js");
 const {validateReview, isLoggedIn,isReviewAuthor} = require("../middleware.js");
 const newReview = require("../controllers/reviews.js");
 
